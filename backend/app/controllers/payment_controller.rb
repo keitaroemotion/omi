b903cfc709e1f02074f03c946c029e612ac381c2
kei <kei@omise.co>
2018-02-03 19:35:31 +0900

    garbage removed

--- backend/app/controllers/payment_controller.rb
+++
@@ -1,6 +0,0 @@
-class PaymentController < ApplicationController
-  def index
-    MailerBird.send_webhook("kei@omise.co").deliver
-    MailerBird.send_webhook("tobasojyo@gmail.com").deliver
-  end
-end
